fe4b635d905ac4d4a836c95f6431101209439fca
an aspirin <beol0127@gmail.com>
2021-12-14 14:47:00 +0000

    chore: update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="zsq",
+    name="z^2",
     version="0.1.0",
-    description="The Z^2 Programming Language",
+    description="The z^2 Programming Language",
     url="https://github.com/JBYT27/zsq",
     author="JBYT27",
     license="Apache 2.0",
