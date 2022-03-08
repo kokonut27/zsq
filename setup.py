8230cd03178c68b82add05e464f4d885ad1f7184
kokomi simp :3 <beol0127@gmail.com>
2022-03-08 14:08:46 +0000

    feat: bump to v0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="z^2",
-    version="0.1.3",
+    version="0.1.4",
     description="The z^2 Programming Language",
     url="https://github.com/kokonut27/zsq",
     author="kokonut27",
--- zsq/info.py
+++ zsq/info.py
@@ -1,4 +1,4 @@
-Version = "0.1.3"
+Version = "0.1.4"
 Author = "kokonut27"
 Website = "https://kokonut27.github.io/zsq"
 Source = "https://github.com/kokonut27/zsq"
